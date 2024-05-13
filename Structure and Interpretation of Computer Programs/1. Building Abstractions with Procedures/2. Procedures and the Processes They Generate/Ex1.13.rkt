7ad731fee33df85e183d041a3e5735bd819047f6
Aniket Mishra <143333884+satiscugcat@users.noreply.github.com>
2024-05-13 12:00:33 +0530

    Rename Structure and Interpretation of Computer Programs/1. Building Abstractions with Procedures/Ex1.13.rkt to Structure and Interpretation of Computer Programs/1. Building Abstractions with Procedures/2. Procedures and the Processes They Generate/Ex1.13.rkt

---
+++ Structure and Interpretation of Computer Programs/1. Building Abstractions with Procedures/2. Procedures and the Processes They Generate/Ex1.13.rkt
@@ -0,0 +1,2 @@
+#lang sicp
+;; I'm skipping this question, since it is covered in much more depth and detail by books on algorithms and data structures.
--- Structure and Interpretation of Computer Programs/1. Building Abstractions with Procedures/Ex1.13.rkt
+++
@@ -1,2 +0,0 @@
-#lang sicp
-;; I'm skipping this question, since it is covered in much more depth and detail by books on algorithms and data structures.
