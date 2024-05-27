f916d0925368979f5edb9c28f1b33dcf0f62e7f8
Aniket Mishra <143333884+satiscugcat@users.noreply.github.com>
2024-05-27 23:57:18 +0530

    Update Ex-02.c

--- The C Programming Language/2. Types, Operators, and Expressions/6. Relational and Logic Operators/Ex-02.c
+++ The C Programming Language/2. Types, Operators, and Expressions/6. Relational and Logic Operators/Ex-02.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 
 int main(){
-    /* I don't know how to do this! If you have a solution, reach out.*/    
+    /* I don't know how to do this! Atleast without using control flow statements that haven't been introduced yet. If you have a solution, reach out.*/    
 }
