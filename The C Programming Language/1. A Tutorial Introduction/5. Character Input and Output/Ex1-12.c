bb35f3c0801db2ca671c28b337139739d9c19690
Aniket Mishra <143333884+satiscugcat@users.noreply.github.com>
2024-06-03 17:09:10 +0530

    Update Ex1-12.c

--- The C Programming Language/1. A Tutorial Introduction/5. Character Input and Output/Ex1-12.c
+++ The C Programming Language/1. A Tutorial Introduction/5. Character Input and Output/Ex1-12.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-int isAlphaNumeric(char c);
+int isAlphaNumeric(int c);
 int main(){
-    char c;
+    int c;
     while((c=getchar())!=EOF){
-        if(isAlphaNumeric(c)==0);
+        if(isAlphaNumeric(c));
         else{
             putchar(c);
             while(isAlphaNumeric(c=getchar())){
@@ -15,6 +15,6 @@ int main(){
     }
 }
 
-int isAlphaNumeric(char c){
-    return (c >='0' && c<='9' || c>='a' && c<='z' ||c>='A' && c<='Z' )? 1 : 0;
+int isAlphaNumeric(int c){
+    return ((c >='0' && c<='9') || (c>='a' && c<='z') || (c>='A' && c<='Z') );
 }
